c780f0f669137a324657284e18d1a95e94c39f27
Illia Polosukhin <ilblackdragon@gmail.com>
2016-03-10 17:04:25 -0800

    Fix #134: change default gpu allocation from 1.0 to 0.95

--- skflow/addons/config_addon.py
+++ skflow/addons/config_addon.py
@@ -30,7 +30,7 @@ class ConfigAddon(object):
             each GPU uniformly on the same machine.
    """
 
-    def __init__(self, num_cores=4, verbose=1, gpu_memory_fraction=1):
+    def __init__(self, num_cores=4, verbose=1, gpu_memory_fraction=0.95):
         gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=gpu_memory_fraction)
         self.config = tf.ConfigProto(log_device_placement=(verbose > 1),
                                      inter_op_parallelism_threads=num_cores,
