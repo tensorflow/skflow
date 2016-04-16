7fc4f260cb97bd8b86801be8a9da787ebfaa1421
Illia Polosukhin <ilblackdragon@gmail.com>
2016-04-16 11:38:52 -0700

    Removing all code that has been moved to Tensorflow. Import skflow from tensorflow now.

--- skflow/__init__.py
+++ skflow/__init__.py
@@ -14,23 +14,13 @@
 #  limitations under the License.
 
 import pkg_resources as pkg_rs
+import warnings
 import numpy as np
-import tensorflow as tf
 
-from skflow.io import *
-from skflow.estimators import *
-from skflow import ops
-from skflow import preprocessing
-from skflow.io import data_feeder
-from skflow import models
-from skflow.trainer import TensorFlowTrainer
+try:
+    from tensorflow.contrib.learn import *
+    warnings.warn("skflow as separate library is deprecated. "
+                  "Use import tensorflow.contrib.learn as skflow instead.", DeprecationWarning)
+except ImportError:
+    raise ImportError("Update your Tensorflow to 0.8+ to use latest skflow.")
 
-__version__, SKLEARN_VERSION, TF_VERSION = \
-[pkg_rs.get_distribution(pkg).version for pkg in ['skflow', 'scikit-learn', 'tensorflow']]
-
-if SKLEARN_VERSION < '0.16.0':
-    raise ImportError("Your scikit-learn version needs to be at least 0.16. "
-                      "Your current version is %s. " % SKLEARN_VERSION)
-if TF_VERSION < '0.7.0':
-    raise ImportError("Your tensorflow version needs to be at least 0.7. "
-                      "Your current version is %s. " % TF_VERSION)
